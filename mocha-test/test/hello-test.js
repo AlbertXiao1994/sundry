3924f775530fc0e4cb6b4116b529d867fa4fdc43
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-11 23:34:02 +0800

    sum测试样例

---
+++ mocha-test/test/hello-test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const sum = require('../hello');
+
+describe('#hello.js', () => {
+    describe('#sum()', () => {
+        it('sum() should return 0', () => {
+            assert.strictEqual(sum(), 0);
+        });
+
+        it('sum() should return 1', () => {
+            assert.strictEqual(sum(1), 1)
+        });
+
+        it('sum() should return 3', () => {
+            assert.strictEqual(sum(1, 2), 3)
+        });
+
+        it('sum() should return 6', () => {
+            assert.strictEqual(sum(1, 2, 3), 6)
+        })
+    })
+})
