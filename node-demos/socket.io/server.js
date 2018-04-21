09778eb532664fbd884f38c9d27b297adc82fd7b
Administrator@XTM-01701112049 <502583304@qq.com>
2018-04-22 00:10:23 +0800

    更新socket.io API版本

--- node-demos/socket.io/server.js
+++ node-demos/socket.io/server.js
@@ -1,33 +1,31 @@
 var express = require('express'),
-    sio = require('socket.io'),
-    request = require('superagent'),
-    bodyParser = require('')
-
-var app = express();
-
-app.use(bodyParser.urlencode({extended: true}));
+    app = express(),
+    path = require('path'),
+    server = require('http').createServer(app)
+    io = require('socket.io')(server);
 
 app.use(express.static("public"));
 
-app.listen(3000);
+server.listen(3000, function() {
+  console.log("Server listening at port 3000");
+});
 
-var io = sio.listen(app),
-    apiKey = 'cc78b550effeea0edb929ada1e9d0729',
-    currentSong,
-    dj;
+var apiKey = 'cc78b550effeea0edb929ada1e9d0729'
+  , currentSong
+  , dj;
 
-function elect(socket) {
+function elect (socket) {
   dj = socket;
   io.sockets.emit('announcement', socket.nickname + ' is the new dj');
   socket.emit('elected');
   socket.dj = true;
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     dj = null;
     io.sockets.emit('announcement', 'the dj left - next one to join becomes dj');
   });
 }
 
-io.socket.on('connection', function(socket) {
+io.on('connection', function(socket) {
   socket.on('join', function(name) {
     socket.nickname = name;
     socket.broadcast.emit('announcement', name + ' joined the chat.');
