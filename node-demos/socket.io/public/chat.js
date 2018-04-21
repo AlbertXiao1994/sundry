d52b5cd9c8b69850c4feca4186c4f340f702e294
Administrator@XTM-01701112049 <502583304@qq.com>
2018-04-21 18:54:19 +0800

    新dj加入时的处理程序

--- node-demos/socket.io/public/chat.js
+++ node-demos/socket.io/public/chat.js
@@ -3,4 +3,14 @@ windos.onload = function() {
   socket.on('connect', function() {
     socket.emit('join', prompt('请输入您的名字：'));
   });
-}
+
+  // 显示聊天框
+  document.getElementById('chat').style.display = 'block';
+
+  socket.on('announcement', function(msg) {
+    var li = document.createElement('li');
+    li.className = 'announcement';
+    li.innnerHTML = msg;
+    document.getElementById('messages').appendChild(li);
+  });
+};
