9a5a718f201a82bdba5cd7f5a7c8a57a07d1c85c
Administrator@XTM-01701112049 <502583304@qq.com>
2018-04-23 19:36:27 +0800

    更新API版本；注册text事件

--- node-demos/socket.io/server.js
+++ node-demos/socket.io/server.js
@@ -17,17 +17,17 @@ var apiKey = 'cc78b550effeea0edb929ada1e9d0729'
 
 function elect (socket) {
   dj = socket;
-  io.sockets.emit('announcement', socket.nickname + ' is the new dj');
+  io.emit('announcement', socket.nickname + ' is the new dj');
   socket.emit('elected');
   socket.dj = true;
   socket.on('disconnect', function () {
     dj = null;
-    io.sockets.emit('announcement', 'the dj left - next one to join becomes dj');
+    io.emit('announcement', 'the dj left - next one to join becomes dj');
   });
 }
 
-io.on('connection', function(socket) {
-  socket.on('join', function(name) {
+io.on('connection', function (socket) {
+  socket.on('join', function (name) {
     socket.nickname = name;
     socket.broadcast.emit('announcement', name + ' joined the chat.');
     if (!dj) {
@@ -37,15 +37,21 @@ io.on('connection', function(socket) {
     }
   });
 
-  socket.on('song', function(currentSong) {
-
+  socket.on('song', function (song) {
+    if (socket.dj) {
+      currentSong = song;
+      socket.broadcast.emit('song', song);
+    }
   });
 
-  socket.on('search', function(q, fn) {
-
+  socket.on('search', function (q, fn) {
+    request('http://tinysong.com/s/' + encodeURIComponent(q)
+      + '?key=' + apiKey + '&format=json', function (res) {
+      if (200 == res.status) fn(JSON.parse(res.text));
+    });
   });
 
-  socket.on('text', function(msg) {
-    
+  socket.on('text', function (msg) {
+    socket.broadcast.emit('text', socket.nickname, msg);
   });
 });
